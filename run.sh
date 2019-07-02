162d0622dbcbca2a81ad9fdfeaf670b60e53d938
Frank Zotynia <frankzotynia10@gmail.com>
2019-07-02 18:13:55 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install docker docker-compose -y
 mkdir ~/appdata ~/appdata/unifi ~/appdata/softether
 cd unifi && docker-compose up -d && cd ../softether && docker-compose up -d
+cd netbox
 

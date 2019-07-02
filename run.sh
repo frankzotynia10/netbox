6e072f931c35e9c0a70ea83c5b20c684c89c368e
Frank Zotynia <frankzotynia10@gmail.com>
2019-07-02 14:57:05 -0400

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install docker docker-compose -y
+mkdir ~/appdata ~/appdata/unifi ~/appdata/softether
+cd unifi && docker-compose up -d && cd ../softether && docker-compose up -d
